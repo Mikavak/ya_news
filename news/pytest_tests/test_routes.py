af474cd67e1524a727753376670d854f463e5090
mi6anya <awvvak@gmail.com>
2024-05-03 11:43:35 +0300

    1

---
+++ news/pytest_tests/test_routes.py
@@ -0,0 +1,10 @@
+from http import HTTPStatus
+from django.urls import reverse
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('news:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
